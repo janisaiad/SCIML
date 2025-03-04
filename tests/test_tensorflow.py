e7bf82430df2efdbb1b508c629dd403431d18bd1
janisaiad <janisaiad.ja@gmail.com>
2025-03-05 00:46:25 +0100

    just reformatting, before training

--- tests/test_tensorflow.py
+++ tests/test_tensorflow.py
@@ -1,14 +1,15 @@
 import tensorflow as tf
 
 
-# check gpu
-if tf.test.is_gpu_available(cuda_only=False, min_cuda_compute_capability=None):
-    print("GPU is available")
-else:
-    print("GPU is not available")
+def test_tensorflow():
+    # check gpu availability
+    if tf.test.is_gpu_available(cuda_only=False, min_cuda_compute_capability=None):
+        print("GPU is available")
+    else:
+        print("GPU is not available")
 
 # check cuda
-if tf.test.is_built_with_cuda():
-    print("CUDA is available")
-else:
-    print("CUDA is not available")
+    if tf.test.is_built_with_cuda():
+        print("CUDA is available")
+    else:
+        print("CUDA is not available")
