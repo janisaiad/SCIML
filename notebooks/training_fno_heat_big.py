0e90ff74301c80986d428698b30a6431c8abe339
janisaiad <janisaiad.ja@gmail.com>
2025-03-15 18:49:22 +0100

    trained well on small networks

--- notebooks/training_fno_heat_big.py
+++ notebooks/training_fno_heat_big.py
@@ -17,7 +17,8 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 tf.config.list_physical_devices('GPU')
-
+import os
+from datetime import datetime
 # +
 nb_xi = 2 # can be deduced from mu because it's len(mu.shape) - 1
 p_1 = 30 # dimension of scheme for xi for all i
@@ -26,9 +27,8 @@ p_3 = 30 # dimension of scheme for xi for all i
 epochs = 100
 index = 450
 n_modes = p_1
-n_layers = 4 # need to be low
+n_layers = 4 # need to be low because there is a low difference between the with time
 alpha = 0.5
-
 activation = 'relu'
 kernel_initializer = 'he_normal'
 device = "GPU"
@@ -42,8 +42,8 @@ n_epochs = epochs
 first_network = tf.keras.Sequential([
     tf.keras.layers.Input(shape=(p_1, p_1,)),  # [batch, p_1, p_1, 3]
     tf.keras.layers.Flatten(),  # [batch, p_1*p_1*3]
-    tf.keras.layers.Dense(512, activation='relu'),
-    tf.keras.layers.Dense(256, activation='relu'),
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dense(32, activation='relu'),
     tf.keras.layers.Dense(p_1 * p_1, activation='relu'),
     tf.keras.layers.Reshape((p_1, p_1,))  # [batch, p_1, p_1, p_2]
 ])
@@ -51,11 +51,12 @@ first_network = tf.keras.Sequential([
 last_network = tf.keras.Sequential([
     tf.keras.layers.Input(shape=(p_1, p_1,)),  # [batch, p_1, p_1, 3]
     tf.keras.layers.Flatten(),  # [batch, p_1*p_1*3]
-    tf.keras.layers.Dense(256, activation='relu'),
-    tf.keras.layers.Dense(512, activation='relu'),
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dense(32, activation='relu'),
     tf.keras.layers.Dense(p_1 * p_1 * (1), activation='linear'),
     tf.keras.layers.Reshape((p_1, p_1,))  # [batch, p_1, p_1, 3]
 ])
+
 
 # first network graph:
 # [batch, p_1, p_1, 3] -> [batch, p_1*p_1*3] -> [batch, 512] -> [batch, 256] -> [batch, p_1*p_1*p_2] -> [batch, p_1, p_1, p_2]
@@ -69,6 +70,21 @@ folder_path = "data/test_data/big_dataset_fno/heat2d/"
 
 model = FNO(regular_params={"first_network": first_network, "last_network": last_network},fourier_params={"n_layers": n_layers, "dim_coords":2, "n_modes": n_modes, "activation": activation, "kernel_initializer": kernel_initializer}, hyper_params={"p_1": p_1, "p_2": p_2,'p_3':p_3,"device": device,"n_epochs":n_epochs,"index":index,"alpha":alpha,"folder_path":folder_path})
 
+os.makedirs('results/fnograph', exist_ok=True)
+
+
+date = datetime.now().strftime("%Y%m%d_%H%M%S")
+
+'''
+tf.keras.utils.plot_model(model, 
+                         to_file=f'results/fno/model_graph_{date}.png',
+                         show_shapes=True, 
+                         show_layer_names=True)
+'''
+# we create a txt file to save the model summary
+
+    
+# print("Model visualization and summary saved to results/fno/")
 # +
 # mus, sol = model.get_data_partial(folder_path,alpha=alpha)
 
@@ -77,14 +93,21 @@ model = FNO(regular_params={"first_network": first_network, "last_network": last
 # print(sol.shape)
 # -
 
-import os
-import logging
+# import logging
 tf.get_logger().setLevel('ERROR')
 # Ajouter en haut du notebook pour désactiver tout le logging
 # logging.getLogger().setLevel(logging.ERROR)  # Ne montrera que les erreurs graves
 
 loss_history_train,loss_history_test = model.fit_partial(save_weights=True)
 
+
+try:
+    with open(f'results/fnograph/model_summary_{date}.txt', 'w') as f:
+        model.summary(print_fn=lambda x: f.write(x + '\n'))
+except Exception as e:
+    print(f"Error saving model summary: {e}")
+    pass
+    
 print(len(loss_history_train))
 print(len(loss_history_test))
 
